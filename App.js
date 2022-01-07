2a5ec0e6bcfeab29975af891cb06916e099136f1
Sharon Jebitok <sharonkosgei4@gmail.com>
2022-01-07 11:58:57 +0300

    Add Changes to Stylesheet

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { Image, StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image source={{uri: "https://i.imgur.com/TkIrScD.png"}} style={{width: 305, height: 159}} />
-      <Text style={{color: "#888", fontSize: 28}}>
+      <Image source={{uri: "https://i.imgur.com/TkIrScD.png"}} style={styles.logo} />
+      <Text style={styles.instructions}>
         To share a photo from your phone with a friend, just press the button below!
       </Text>
       <StatusBar style="auto" />
@@ -20,4 +20,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10,
+  },
+  
+  instructions: {
+    color: "#888", 
+    fontSize: 28
+  }
 });
