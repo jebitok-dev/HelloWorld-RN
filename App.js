da9319f24212368a5ec7c9ea2ed62db2124d715c
Sharon Jebitok <sharonkosgei4@gmail.com>
2022-01-07 11:54:20 +0300

    Add Changes to App

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import logo from "./assets/logo.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Image source={logo} style={{width: 305, height: 159}} />
+      <Text style={{color: "#888", fontSize: 28}}>
+        To share a photo from your phone with a friend, just press the button below!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
