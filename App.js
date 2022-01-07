bb7e6e941e1bd4f702f523b878cf032367480319
Sharon Jebitok <sharonkosgei4@gmail.com>
2022-01-07 13:05:07 +0300

    Add Changes to Image Sharing

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React, {useState} from "react";
 import { TouchableOpacity, Image, StyleSheet, Text, View } from 'react-native';
 import * as ImagePicker from "expo-image-picker";
-import { setSelectedLog } from 'react-native/Libraries/LogBox/Data/LogBoxData';
+import * as Sharing from "expo-sharing"
 
 export default function App() {
   const [selectedImage, setSelectedImage] = React.useState(null);
@@ -24,6 +24,14 @@ export default function App() {
     setSelectedImage({localUri: pickerResult.uri});
   };
 
+  let openShareDialogAsync = async() => {
+    if(!(await Sharing.isAvailableAsync())) {
+      alert(`Uh oh, sharing isn't available on your platform`);
+      return;
+    }
+    await Sharing.shareAsync(selectedImage.localUri);
+  }
+
   if (selectedImage !== null) {
     return (
       <View style={styles.container}>
@@ -31,6 +39,9 @@ export default function App() {
           source={{uri: selectedImage.localUri}}
           style={styles.thumbnail}
         />
+        <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
+          <Text style={styles.buttonText}>Share this photo</Text>
+        </TouchableOpacity>
       </View>
     )
   }
